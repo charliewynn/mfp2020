1aeb8e25a211b97de58a124864ba463dca196adf
Jordan Miller <jordan.miller@bluescopebuildingsna.com>
2020-09-16 10:49:59 -0500

    added table to display score predictions

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,8 +3,53 @@ import React from "react";
 function Player({ name, scores, clicked }) {
   return (
     <div className="player">
-      {name}
-      <div>{JSON.stringify(scores)}</div>
+      <table>
+        <th>{name}'s Predictions</th>
+        <tr>
+          <th>OU Score</th>
+          <th>Opponent Score</th>
+        </tr>
+        <tr>
+          <td>{JSON.stringify(scores[0].ou)}</td>
+          <td>{JSON.stringify(scores[0].opp)}</td>
+        </tr>
+        <tr>
+          <td>{JSON.stringify(scores[1].ou)}</td>
+          <td>{JSON.stringify(scores[1].opp)}</td>
+        </tr>
+        <tr>
+          <td>{JSON.stringify(scores[2].ou)}</td>
+          <td>{JSON.stringify(scores[2].opp)}</td>
+        </tr>
+        <tr>
+          <td>{JSON.stringify(scores[3].ou)}</td>
+          <td>{JSON.stringify(scores[3].opp)}</td>
+        </tr>
+        <tr>
+          <td>{JSON.stringify(scores[4].ou)}</td>
+          <td>{JSON.stringify(scores[4].opp)}</td>
+        </tr>
+        <tr>
+          <td>{JSON.stringify(scores[5].ou)}</td>
+          <td>{JSON.stringify(scores[5].opp)}</td>
+        </tr>
+        <tr>
+          <td>{JSON.stringify(scores[6].ou)}</td>
+          <td>{JSON.stringify(scores[6].opp)}</td>
+        </tr>
+        <tr>
+          <td>{JSON.stringify(scores[7].ou)}</td>
+          <td>{JSON.stringify(scores[7].opp)}</td>
+        </tr>
+        <tr>
+          <td>{JSON.stringify(scores[8].ou)}</td>
+          <td>{JSON.stringify(scores[8].opp)}</td>
+        </tr>
+        <tr>
+          <td>{JSON.stringify(scores[9].ou)}</td>
+          <td>{JSON.stringify(scores[9].opp)}</td>
+        </tr>
+      </table>
     </div>
   );
 }
