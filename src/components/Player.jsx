c973fcf0551ea49651ecf0e65bd5ee1eb8d0bbe9
Jordan Miller <jordan.miller@bluescopebuildingsna.com>
2020-09-16 11:25:36 -0500

    Added some style

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,14 +2,16 @@ import React from "react";
 
 function Player({ name, scores, clicked }) {
   return (
-    <table>
-      <th>{name}'s Predictions</th>
-      <tr>
-        <th>OU Score</th>
-        <th>Opponent Score</th>
-      </tr>
-      {scores.map(column => <tr><td>{column.ou}</td><td>{column.opp}</td></tr>)}
-    </table>
+    <div>
+      <h3>{name}'s Predictions</h3>
+      <table class="playerPredictions">
+        <tr>
+          <th>OU Score</th>
+          <th>Opp Score</th>
+        </tr>
+        {scores.map(column => <tr><td>{column.ou}</td><td>{column.opp}</td></tr>)}
+      </table>
+    </div>
   );
 }
 
